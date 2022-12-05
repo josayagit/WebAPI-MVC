76b15822ed1bc6f2c12b8dd563b1ce99b3e93700
Josaya Mokgobu <105625930+josayagit@users.noreply.github.com>
2022-12-05 03:47:43 +0000

    Add files via upload

---
+++ SoftServeProject/SoftServeProject/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftServeProject.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public String LastName { get; set; }
+        public Email MyProperty { get; set; }
+    }
+}
